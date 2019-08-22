6ebc8647ca1a26a6440d805a33f99873f33d0e8c
manoj7git <43413379+manoj7git@users.noreply.github.com>
2019-08-23 00:00:56 +0530

    RetryAnalyzer Script updated for retrying execution of failed TC's, RetryTransformer under Listners & RetryAnalyzer class under Utilities

---
+++ VRT/src/main/java/com/vrt/Listners/RetryTransformer.java
@@ -0,0 +1,18 @@
+package com.vrt.Listners;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import com.vrt.utility.RetryAnalyzer;
+
+public class RetryTransformer implements IAnnotationTransformer{
+
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		
+		annotation.setRetryAnalyzer(RetryAnalyzer.class);		
+	}
+
+}
